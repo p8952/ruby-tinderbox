bcb3af332a6cca0829ec8ca4799de7b9e0b1e766
Peter Wilmott <p@p8952.info>
2014-10-16 20:40:28 +0100

    Keep emerge quiet

--- conf/provision.sh
+++ conf/provision.sh
@@ -4,9 +4,9 @@ set -o errexit
 sudo cp /vagrant/conf/make.conf /etc/portage/make.conf
 
 RUBIES="dev-lang/ruby:1.9 dev-lang/ruby:2.0 dev-lang/ruby:2.1"
-sudo emerge --pretend $RUBIES
+sudo emerge --pretend --quiet $RUBIES
 if [[ $? == 1 ]]; then
        sudo emerge --autounmask-write $RUBIES
        sudo etc-update --automode -5
 fi
-sudo emerge --noreplace $RUBIES
+sudo emerge --noreplace --quiet $RUBIES

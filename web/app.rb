5f6f1e91785c2d4ed4cc09a436d7f0ae0bac8b37
Peter Wilmott <p@p8952.info>
2014-10-19 19:32:55 +0100

    Split into three pages

--- web/app.rb
+++ web/app.rb
@@ -8,7 +8,19 @@ class RubyStats < Sinatra::Base
 	packages = DB[:packages]
 
 	get '/' do
-		erb :home, :locals => {:packages => packages}
+		redirect to('/ruby_targets')
+	end
+
+	get '/ruby_targets' do
+		erb :ruby_targets, :locals => {:packages => packages}
+	end
+
+	get '/outdated_gems' do
+		erb :outdated_gems, :locals => {:packages => packages}
+	end
+
+	get '/build_status' do
+		erb :build_status, :locals => {:packages => packages}
 	end
 
 end

9af676106d0adb83902d76b342f1bdb0422c2a60
Peter Wilmott <p@p8952.info>
2014-12-08 21:16:52 +0000

    Use DATABASE_URL; don't map builds to packages

--- web/lib/models.rb
+++ web/lib/models.rb
@@ -1,9 +1,8 @@
-DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://db/database.sqlite3', pool_timeout: 25)
+DB = Sequel.connect(ENV['DATABASE_URL'], pool_timeout: 25)
 
 class Package < Sequel::Model
 	one_to_many :builds
 end
 
 class Build < Sequel::Model
-	many_to_one :package
 end

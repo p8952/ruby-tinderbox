fa831d15db068ba562cde7c401f6693697ff41dc
Peter Wilmott <p@p8952.info>
2014-12-08 21:18:03 +0000

    Rename update.rb to packages.rb and refactor

---
+++ web/lib/packages.rb
@@ -0,0 +1,24 @@
+def update_packages
+	packages_txt = `python3 lib/packages.py`
+	packages = DB[:packages]
+	packages.delete
+	packages_txt.lines.each do |line|
+		DB.transaction do
+			category, name, version, revision, slot, r19_target, r20_target, r21_target = line.split(' ')
+			gem_version = Gems.info(name)['version']
+			gem_version = 'nil' if gem_version.nil?
+			Package.create(
+				category: category,
+				name: name,
+				version: version,
+				revision: revision,
+				slot: slot,
+				identifier: category + '/' + name + '-' + version + (revision == 'r0' ? '' : "-#{revision}"),
+				gem_version: gem_version,
+				r19_target: r19_target,
+				r20_target: r20_target,
+				r21_target: r21_target
+			)
+		end
+	end
+end

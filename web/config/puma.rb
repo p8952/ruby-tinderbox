a11e2da45a32410f340755674d2b05c735e9579f
Peter Wilmott <p@p8952.info>
2014-12-09 00:19:23 +0000

    Improve puma config

--- web/config/puma.rb
+++ web/config/puma.rb
@@ -1,7 +1,9 @@
-workers Integer(ENV['PUMA_WORKERS'] || 5)
 threads Integer(ENV['MIN_THREADS'] || 1), Integer(ENV['MAX_THREADS'] || 16)
+workers Integer(ENV['PUMA_WORKERS'] || 4)
 
-preload_app!
-
-port ENV['PORT'] || 9292
 environment ENV['RACK_ENV'] || 'development'
+port ENV['PORT'] || 9292
+
+daemonize
+pidfile 'var/run/puma.pid'
+stdout_redirect 'var/log/puma.log', 'var/log/puma_error.log'

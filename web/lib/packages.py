949752d9227d6feb8b4a0591f484669eaacd1f45
Peter Wilmott <p@p8952.info>
2014-12-20 22:59:15 +0000

    Redo ebd2a7

--- web/lib/packages.py
+++ web/lib/packages.py
@@ -9,20 +9,19 @@ for package in portdb.cp_all():
 		slot_dict.setdefault(slot, {})[package_v] = (iuse, inherited)
 	for slot, package_v_dict in slot_dict.items():
 		if 'ruby_targets_' in iuse:
-                        package_v_best = portage.best(list(package_v_dict))
-                        category, pkgname, version, revision = portage.catpkgsplit(package_v_best)
-                        if category == 'dev-ruby':
-                            print(category + ' ' + pkgname + ' ' + version + ' ' + revision + ' ' + slot, end=' ')
-                            if 'ruby_targets_ruby19' in iuse:
-                                    print('ruby19', end=' ')
-                            else:
-                                    print('nil', end=' ')
-                            if 'ruby_targets_ruby20' in iuse:
-                                    print('ruby20', end=' ')
-                            else:
-                                    print('nil', end=' ')
-                            if 'ruby_targets_ruby21' in iuse:
-                                    print('ruby21', end=' ')
-                            else:
-                                    print('nil', end=' ')
-                            print()
+			package_v_best = portage.best(list(package_v_dict))
+			category, pkgname, version, revision = portage.catpkgsplit(package_v_best)
+			print(category + ' ' + pkgname + ' ' + version + ' ' + revision + ' ' + slot, end=' ')
+			if 'ruby_targets_ruby19' in iuse:
+				print('ruby19', end=' ')
+			else:
+				print('nil', end=' ')
+			if 'ruby_targets_ruby20' in iuse:
+				print('ruby20', end=' ')
+			else:
+				print('nil', end=' ')
+			if 'ruby_targets_ruby21' in iuse:
+				print('ruby21', end=' ')
+			else:
+				print('nil', end=' ')
+			print()
